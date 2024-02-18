83001acfb94f9fbc312136e25173d5560818d7b4
@aman78612 <amansarkaraman1@gmail.com>
2024-02-18 21:27:33 +0600

    admin dashboard attached

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -8,7 +8,13 @@
         <meta name="author" content="" />
         <title>Dashboard - SB Admin</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="assets/css/styles.css" rel="stylesheet" />
+        <link href="asset/css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
+        <?php include('nav.php') ?>
+            <div id="layoutSidenav">
+                <?php include('sidebar.php') ?>
+                    <div id="layoutSidenav_content">
+                        <main>
+                        </main>
